a780425c2400d697e45098234ddf793c41d45d42
Nils Ponsard <nilsponsard@gmail.com>
2020-06-16 19:32:32 +0200

    affichage terminé

--- Main.asm
+++ Main.asm
@@ -4,7 +4,7 @@ N:	.word 8				# const unsigned N = 0
 SolutionNum: 
 	.word 0				# unsigned SolutionNum = 0
 Colonne:
-	.word 1,2,3,4,5,6,7,8 		# unsigned Colonne[N] (rempli avec des valeurs de test)
+	.word 0,1,2,3,4,5,6,7 		# unsigned Colonne[N] (rempli avec des valeurs de test)
 StrSolution:
 	.asciiz "Solution numero " 	# constante texte
 StrDeuxPoints:
@@ -20,8 +20,14 @@ StrSetwDeux:
 	.text
 
 # Main
-
-
+main:
+# chargement des valeurs pour le test
+	la $a0,Colonne
+	la $a1,N	
+	lw $a1,0($a1)
+	jal AfficherSolution
+	ori $v0,$zero,10 		# $v0 <− 10
+	syscall				# return 0
 
 # AfficherSolution : $a0: pointeur vers le tableau, $a1 : numéro de la solution N
 AfficherSolution:
@@ -30,9 +36,12 @@ AfficherSolution:
 	ori $v0,$zero,4			# $v0 <- 4
 	la $a0,StrSolution		# $a0 <- StrSolution
 	syscall 			# affiche "Solution numero"
+	la $t9,SolutionNum		# chargement de l’adresse où est stocké SolutionNum dans $t9
+	lw $a0,0($t9)			# $a0 <- SolutionNum
+	addi $a0,$a0,1			# ++SolutionNum
+	sw $a0,0($t9)			# on stocke la nouvelle valeur de SolutionNum
 	ori $v0,$zero,1 		# $v0 <- 1
-	or $a0,$zero,$t1		# $a0 <- N
-	syscall				# cout << N
+	syscall				# cout << SolutionNum
 	ori $v0,$zero,4			# $v0 <- 4
 	la $a0,StrDeuxPoints		# $a0 <- StrDeuxPoints
 	syscall 			# affciche ':'
@@ -46,9 +55,13 @@ AS_For_i:
 	or $t4,$zero,$zero		# j ($t4) <- 0
 AS_For_j:
 	slt $t5,$t4,$t1			# $t5 = (j<N)
-	beq $t5,$zero,AS_Fin_For_j	# si (i>=N) on termine la boucle
+	beq $t5,$zero,AS_Fin_For_j	# si (j>=N) on termine la boucle
+	
+	sll $t6,$t3,2			# $t6 <- i*4
+	add $t6,$t0,$t6			# $t6 (emplacement de la case à charger) <- $t0 (@Colonne) + offset
+	lw $t7,0($t6)			# $t7 <- Colonne[i]
 
-	bne $t5, 			# if (j == Colonne[i])
+	bne $t4,$t7,AS_Else		# if (j == Colonne[i])
 	
 	ori $v0,$zero,4			# $v0 <- 4
 	la $a0,StrSetwDeux		# $a0 <- StrSetwDeux
@@ -73,6 +86,9 @@ AS_Fin_For_j:
 	j AS_For_i
 
 AS_Fin_For_i:
+	# TODO:restaurer les valeurs de $a0 et $a1
+	jr $ra
+	
 	
 		
 
